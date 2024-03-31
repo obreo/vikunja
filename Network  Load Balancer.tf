e770b413b4095c8349ab588870bccbbcfd89364b
Ibrahim <ibra.653@gmail.com>
2024-03-31 11:04:21 +0500

    Update Network  Load Balancer.tf

--- Network  Load Balancer.tf
+++ Network  Load Balancer.tf
@@ -45,7 +45,11 @@ resource "aws_lb_target_group" "green" {
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/lb_listener
 resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.load_balancer.arn
-  port              = "80" # Single listener is allowed for ECS; 80 for s3 & 443 for Cloudfront. The listner's should be assigned by ECS service as well. 
+  port              = "80" # Single listener is allowed for ECS; 80 for s3 over HTTP & 443 for Cloudfront over HTTPS. However, if HTTPS is used then it is recmmended to use 
+                           # ALB that listens to port 443 - so it terminates the ssl before forwarding it to the target group - and make the target group use port 80 
+                           # and adjust the vikunja to use the same port. As NLB terminates the ssl cert in layer 4, at the backend of the instances, 
+                           # means the instance needs an ssl cert installed and used for the vikunja while adjusting the app to listen to port 443. 
+                           # A workaround - which is not efficient, is to connect cloudfront of https to the nlb that will be used as a backend endpoint.
   protocol          = "TCP"
 
   default_action {
